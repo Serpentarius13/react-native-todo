1cd1a926d81c91e42f7a0774101f8621fc3f4c33
Serpentarius13 <serpentarium13@mail.ru>
2022-11-13 00:33:49 +0300

    final todo

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
-import { StatusBar } from "expo-status-bar";
+//! WARNING! THIS CODE IS PURE SHIT! LOOK INSIDE ONLY IF YOU WANT TO HAVE YOUR EYES BURNED!
+
 import {
+  //! List of components used
   StyleSheet,
   Text,
   TextInput,
@@ -7,6 +9,9 @@ import {
   Button,
   Pressable,
   ScrollView,
+  FlatList, //! Fuck this mofo
+  Modal,
+  Image,
 } from "react-native";
 import { useState, useRef } from "react";
 
@@ -15,30 +20,40 @@ export default function App() {
   const [text, setText] = useState(""); //! Temp text
   const [removed, setRemoved] = useState(0); //! Removed goals
 
+  const [modVis, setModVis] = useState(false); //! Modal
+
   const inputHandle = (someText) => {
     //! Updating input
+
     setText(someText);
   };
 
   const addGoal = (goal) => {
+    //! Adding goal
+
     goal = [goal, null];
     putGoal([...goalArr, goal]);
+    setText("");
   };
 
   const strokeGoal = (goal) => {
+    //! Stroking goal
+
     const index = goalArr.indexOf(goal);
-    const textDecor = goalArr[index][1]
+    const textDecor = goalArr[index][1] //! Look at this motherfucker
       ? null
       : {
           textDecorationLine: "line-through",
-          color: 'black'
+          color: "black",
         };
     goalArr[index] = [goalArr[index][0], textDecor];
     putGoal([...goalArr]);
   };
 
   const removeGoal = (goal) => {
-    const index = goalArr.indexOf(goal);
+    //! Removing goal
+
+    const index = goalArr.indexOf(goal);  //! Finding it
     goalArr.splice(index, 1);
     putGoal([...goalArr]);
     setRemoved((count) => {
@@ -47,18 +62,56 @@ export default function App() {
   };
 
   const removeAll = () => {
+    //! Removing them all
+
     putGoal([]);
+
+    setModVis((vis) => true); //! Opening modal
+
+    console.log(modVis);
+
+    setTimeout(() => setModVis(false), 3000); //! Closing modal
   };
 
   return (
-    <View style={styles.container}>
-      <TextInput
+    <View style={styles.container}> //! Global container 
+      <Modal animationType="slide" visible={modVis}> //! Modal container !animationType! !visible!
+        <View style={styles.modal}>
+          <Image source={require("./assets/floppa.png")}></Image> //! Image container !source!
+          <Text
+            style={{
+              ...styles.text,
+              color: "white",
+              fontSize: 30,
+              marginLeft: 0,
+              marginTop: 24,
+            }}
+          >
+            {" "}
+            You have completed{" "}
+            <Text
+              style={{
+                ...styles.text,
+                color: "white",
+                fontSize: 48,
+                marginLeft: "0",
+                marginTop: 24,
+              }}
+            >
+              {" "}
+              {removed}{" "}
+            </Text>{" "}
+            tasks! Cool!{" "}
+          </Text>
+        </View>
+      </Modal>
+      <TextInput //! Input
         style={styles.input}
         placeholder="type here"
         onChangeText={(data) => inputHandle(data)}
         value={text}
       ></TextInput>
-      <Button onPress={(data) => addGoal(text)} title="Add goal"></Button>
+      <Button onPress={(data) => addGoal(text)} title="Add goal"></Button> //!Button !title!
       <View
         style={{
           width: 390,
@@ -67,39 +120,48 @@ export default function App() {
           alignItems: "center",
         }}
       >
-        <ScrollView alwaysBounceVertical={false} contentContainerStyle={styles.container}>
-          {goalArr &&
-            goalArr.map((el) => (
-              <View key={Math.random() * 10000} style={styles.goal}>
-                <Pressable style={{position: "absolute", top: '25%', left: '-6%'}}onPress={(data) => removeGoal(el)}>
-                  <Text
-                    style={{
-                      ...styles.goalText,
-                      fontWeight: "normal",
-                      fontSize: 24,
-                    }}
-                  >
-                    {" "}
-                    X{" "}
-                  </Text>
-                </Pressable>
+        <ScrollView //! Scroll view
+          alwaysBounceVertical={false} //! !Bounce property!
+          contentContainerStyle={styles.container} //! Styling only with !contentContainerStyle!
+        >
+          {goalArr.map((el) => (
+            <View key={Math.random() * 123456} style={styles.goal}> //! Unique key implementation (-.-)
+              <Pressable
+                style={{ position: "absolute", top: "25%", left: "-6%" }}
+                onPress={(data) => removeGoal(el)}
+              >
                 <Text
-                  style={
-                    el[1]
-                      ? Object.assign({ ...styles.goalText }, { ...el[1] })
-                      : styles.goalText
-                  }
-                  onPress={(data) => strokeGoal(el)}
+                  style={{
+                    ...styles.goalText,
+                    fontWeight: "normal",
+                    fontSize: 24,
+                  }}
                 >
-                  {el[0]}
+                  {" "}
+                  X{" "}
                 </Text>
-              </View>
-            ))}
+              </Pressable>
+              <Text
+                style={ //! Crazy part two
+                  el[1]
+                    ? Object.assign({ ...styles.goalText }, { ...el[1] })
+                    : styles.goalText 
+                }
+                onPress={(data) => strokeGoal(el)}
+              >
+                {el[0]}
+              </Text>
+            </View>
+          ))}
         </ScrollView>
       </View>
 
-      <Pressable style={styles.endButton} onPress={(data) => removeAll()}>
-        <Text style={{ color: "white", fontSize: 24 }}> Remove all </Text>
+      <Pressable style={styles.endButton} onPress={(data) => removeAll()}> //! Pressable
+        <Text style={{ color: "white", fontSize: 24 }}>
+          {" "}
+          Remove all {"\n"}{" "}
+          <Text style={{ color: "white", fontSize: 12 }}> (click this) </Text>
+        </Text>
       </Pressable>
       <Text
         onPress={(data) => setRemoved(0)}
@@ -119,7 +181,7 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
+export const styles = StyleSheet.create({ //! Stylesheet
   container: {
     alignItems: "center",
     width: 390,
@@ -153,7 +215,7 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     marginLeft: 24,
     textAlign: "center",
-    color: 'white',
+    color: "white",
   },
 
   endButton: {
@@ -173,6 +235,14 @@ const styles = StyleSheet.create({
     color: "white",
     marginTop: 10,
     width: "80%",
-    position: 'relative',
+    position: "relative",
+  },
+
+  modal: {
+    width: "100%",
+    height: "100%",
+    backgroundColor: "#845ef7",
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
